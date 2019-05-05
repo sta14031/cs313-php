5e4154120c6d17ea9c48515933dff86339c33b7d
Tyler <sta14031@byui.edu>
2019-05-04 22:14:40 -0600

    Added my personal homepage

--- web/index.php
+++ web/index.php
@@ -1,5 +1,43 @@
-<?php
-
-  phpinfo();
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" type="text/css" href="homepage.css">
+  <title>Tyler&apos;s Home Page</title>
+</head>
+<body>
+  <header>
+    <nav>
+      <a href="https://www.byui.edu/">BYU-I</a> |
+      <a href="https://www.w3schools.com">W3Schools</a> |
+      <a href="https://www.nethack.org/">NetHack</a> |
+      <a href="https://www.minecraft.net/">Minecraft</a>
+    </nav>
+  </header>
+  <div id="title">
+    <img src="me.png" alt="My beautiful face" />
+    <h2>Welcome to</h2>
+    <h1>TYLER STARR&apos;S</h1>
+    <h2>home page</h2>
+  </div>
+  <div id="content">
+    <p>My name is Tyler - welcome to my home page!
+      I&apos;m a computer science student at BYU-Idaho in my junior year. I enjoy programming and software development,
+      as well as website development. I enjoy cooking, baking, playing the piano, and playing video games! See the 
+      navbar at the top of this page for links to two of my favorite games.
+    </p>
+    <p>
+      The current time on the server is <b>
+        <?php
+          $hour = $info['hours'];
+          $min = $info['minutes'];
+          $sec = $info['seconds'];
+          
+          echo "$hours:$minutes:$seconds";
+       ?></b>.
+    </p>
+  </div>
+</body>
+</html>
