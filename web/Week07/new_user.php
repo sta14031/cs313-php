3f75505d64f9845ea4a94e2414e1547067952956
Tyler <sta14031@byui.edu>
2019-06-04 16:09:03 -0600

    Trying without the include...

--- web/Week07/new_user.php
+++ web/Week07/new_user.php
@@ -1,15 +1,36 @@
 <?php
 session_start();
 
-require("db.php");
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+//require("db.php");
 
 $username = $_POST["username"];
 $password = $_POST["password"];
 
 $hashedpw = password_hash($password);
 
-$db->query("INSERT INTO Activity7Users (UserName, UserPassword)
-    VALUES ($username, $hashedpw)");
+$db->query("INSERT INTO Activity7Users (UserName, UserPassword) VALUES ($username, $hashedpw)");
 
 $_SESSION["user"] = $db->lastInsertId();
 
