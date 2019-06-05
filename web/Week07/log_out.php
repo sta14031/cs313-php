5016e7617ba8a5b12676aa65fe36937f10191bf5
Tyler <sta14031@byui.edu>
2019-06-04 19:05:24 -0600

    Changed the logout page to use session_destroy()

--- web/Week07/log_out.php
+++ web/Week07/log_out.php
@@ -1,5 +1,5 @@
 <?php
-session_abort();
+session_destroy();
 header("Location: welcome.php");
 die();
 ?>
--- web/Week07/new_user.php
+++ web/Week07/new_user.php
@@ -11,7 +11,7 @@ $hashedpw = password_hash($password);
 // Does this username already exist in the page?
 $stmt = $db->prepare("SELECT * FROM Activity7Users WHERE UserName ILIKE '" . $username . "'");
 $stmt->execute();
-echo "Queried the database\n";
+
 if ($stmt->rowCount() > 0) {
 
     header("Location: sign_in.php?error=badpw");
@@ -23,8 +23,6 @@ $stmt = $db->prepare("INSERT INTO Activity7Users (UserName, UserPassword) VALUES
 $stmt->bindValue(':username', $username, PDO::PARAM_STR);
 $stmt->bindValue(':hashedpw', $hashedpw, PDO::PARAM_STR);
 $stmt->execute();
-
-echo "Inserted into the database\n";
 
 $_SESSION["user"] = $db->lastInsertId();
 
